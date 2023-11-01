80506ed17c0b6bd1f3f07b44edada0062c79b31e
edris-poursadeghi <edris.emperator@gmail.com>
2023-11-01 18:27:01 +0330

    main add git ignore.ir

--- test/example.js
+++ test/example.js
@@ -1,4 +1,4 @@
-const findPropertyValue = require('./index');
+const findPropertyValue = require('../index');
 
 const data = [{ name: 'John', age: 25 }, { name: 'Jane', age: 30 }];
 const property = 'age';
